6c463840694b57ecc8d3cdac7a5843551d9b616b
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-09-24 17:37:59 +0600

    React simple

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555; 
+const singer = {name: 'Dr. Mahfuj', job: 'Faltu song'}
+
+const singerStyle={
+  color: 'green'
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello Dude.. React js!</h3>
+        </div>
+        <div className="music">
+          <p>Name: {2222 + number}</p>
+          <p style={singerStyle}>Name: {singer.name} Job: {singer.job}</p>
+        </div>
+        <h1 className='singer'>OK</h1>
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
